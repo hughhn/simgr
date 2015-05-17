dbac0d97d75f53848d4f21e9a5f805e46a6c9eb7
Jonathan Ong <me@jongleberry.com>
2015-05-17 11:47:44 -0700

    move convert ffmpeg

---
+++ lambda/convert-ffmpeg.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var exec = require('mz/child_process').execFile
+
+module.exports = function (filename, metadata, options, settings) {
+  var format = options.format
+  var width = metadata.width
+  var height = metadata.height
+  var opts = settings[format]
+  var maxrate = (opts.bitratefactor || 5) * width * height
+
+  var args = [
+    '-y',
+    '-loglevel',
+    'error',
+    '-i',
+    filename,
+    '-c:v',
+    format === 'webm' ? 'libvpx' : 'libx264',
+    '-crf',
+    opts.crf || 20,
+    // We always want it to act as a maxrate
+    format === 'webm' ? '-b:v' : '-maxrate',
+    maxrate,
+  ]
+
+  if (format === 'mp4') {
+    args.push(
+      // For whatever reason this colorspace is required
+      '-pix_fmt',
+      'yuv420p',
+      '-bufsize',
+      // Not sure what to set, so just set it to 5x the maxrate
+      5 * maxrate
+    )
+
+    // No side can be odd, so if one is odd,
+    // we'll just double the size.
+    // May lose some color,
+    // but these are GIFs anyways
+    if (width % 2 || height % 2) args.push('-vf', 'scale=iw*2:ih*2')
+  }
+
+  return exec('ffmpeg', args.concat(options.out))
+}
